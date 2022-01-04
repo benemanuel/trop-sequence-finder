7f3234f39593c76510afe5a9d7bd832ffda69242
jaykaron <jaykaron@gmail.com>
2022-01-03 20:47:40 -0500

    Add occurrences to main sequence

--- tropsequences-localcompute.js
+++ tropsequences-localcompute.js
@@ -61,7 +61,6 @@ var linkline = function (d) {
 
 var probformat = d3.format(".1%");
 var countformat = d3.format(",");
-
 var tropnames = d3.map([{ "char": "\u0597", "name": "revii", "heb": "רְבִיעִ֗י" }, { "char": "\u059d", "name": "gereshmukdam" }, { "char": "\u05a6", "name": "merkhakfula", "heb": "מֵרְכָא־כְפוּלָ֦ה" }, { "char": "\u059e", "name": "gershayim", "heb": "גֵּרְשַׁ֞יִם" }, { "char": "\u059b", "name": "tevir", "heb": "תְּבִ֛יר" }, { "char": "\u059f", "name": "karnepara", "heb": "קַרְנֵי פָרָ֟ה" }, { "char": "\u0595", "name": "gadol", "heb": "גָּד֕וֹל" }, { "char": "\u05a0", "name": "telishagedola", "heb": "תְּ֠לִישָא גְדוֹלָה" }, { "char": "\u0599", "name": "pashta", "heb": "פַּשְׁטָא֙" }, { "char": "\u0593", "name": "shalshelet", "heb": "שַׁלְשֶׁ֓לֶת" }, { "char": "\u0596", "name": "tipkha", "heb": "טִפְּחָ֖א" }, { "char": "\u059a", "name": "yetiv", "heb": "יְ֚תִיב" }, { "char": "\u0592", "name": "segol", "heb": "סֶגוֹל֒" }, { "char": "\u05aa", "name": "yerakhbenyomo", "heb": "יֵרֶח בֶּן יוֹמ֪וֹ" }, { "char": "\u05ae", "name": "zarka", "heb": "זַרְקָא֮" }, { "char": "\u05a3", "name": "munakh", "heb": "מֻנַּ֣ח" }, { "char": "\u05a5", "name": "merkha", "heb": "מֵרְכָ֥א" }, { "char": "\u05a8", "name": "kadma", "heb": "קַדְמָ֨א" }, { "char": "\u0591", "name": "etnakhta", "heb": "אֶתְנַחְתָּ֑א" }, { "char": "\u05c3", "name": "sofpasuk", "heb": "סוֹף פָּסוּק׃" }, { "char": "\u0598", "name": "tsinnorit", "heb": "צִנּוֹרִת֘" }, { "char": "\u059c", "name": "geresh", "heb": "גֵּ֜רֵשׁ" }, { "char": "\u05a9", "name": "telishaketana", "heb": "תְּלִישָא קְטַנָּה֩" }, { "char": "\u05a7", "name": "darga", "heb": "דַּרְגָּ֧א" }, { "char": "\u05a1", "name": "pazer", "heb": "פָּזֵ֡ר" }, { "char": "\u05a4", "name": "mapakh", "heb": "מַהְפַּ֤ך" }, { "char": "\u0594", "name": "katan", "heb": "קָטָ֔ן" }], function (t) { return t.name; });
 
 var tropstrings;
@@ -100,7 +99,7 @@ function init(root) {
         d.x = x(d.depth * (hspace + tree.nodeSize()[0])) - tree.nodeSize()[0];
         d.y = i * (vspace + tree.nodeSize()[1]); // - d.depth*(vspace + tree.nodeSize()[1]);
 
-        d.prob = d.count / depthsum || 0;
+        d.prob = (d.count / depthsum) || 0;
     });
     links = tree.links(nodes);
 
@@ -167,14 +166,14 @@ function update() {
 
 
     node.select("rect.histbar")
-        .attr("x", function (d) { return tree.nodeSize()[0] - tree.nodeSize()[0] * d.prob; })
-        .attr("width", function (d) { return tree.nodeSize()[0] * d.prob; });
+        .attr("x", (d) => tree.nodeSize()[0] - tree.nodeSize()[0] * d.prob)
+        .attr("width", (d) => tree.nodeSize()[0] * d.prob);
 
     node.select("text.count")
-        .text(function (d) { return countformat(d.count); });
+        .text((d) => countformat(d.count));
 
     node.select("text.prob")
-        .text(function (d) { return probformat(d.prob); });
+        .text((d) => probformat(d.prob));
 
     node
         .attr("transform", oldypos)
@@ -234,7 +233,7 @@ function applySearchSeq() {
     const searchString = searchSeq.map((trop) => trop.char).join("");
     const searchText = searchSeq.map((trop) => trop.heb).join(" ");
 
-    $("#searchSeqContainer").text(searchText);
+    $("#searchSeq").text(searchText);
 
     nodes.forEach((node) => {
         var exp = RegExp(frombeginningprefix() + searchString + node.char, "g");
@@ -244,11 +243,12 @@ function applySearchSeq() {
             .map((p) => p.trop.match(exp).length));
         node.disabled = !node.count;
     });
-    const totalCount = d3.sum(nodes.map((node) => node.count));
+    const totalCountOfContinuations = d3.sum(nodes.map((node) => node.count));
     nodes.forEach((node) => {
-        node.prob = node.count / totalCount;
+        node.prob = (node.count / totalCountOfContinuations) || 0;
     });
 
+    let totalOccurrences = 0;
     // do graph location data
     disaggregated = [];
     var exp = RegExp(frombeginningprefix() + searchString, "g");
@@ -261,9 +261,18 @@ function applySearchSeq() {
 
         var thematch = p.trop.match(exp);
         pasukobj.count = thematch ? thematch.length : 0;
-
+        totalOccurrences += pasukobj.count;
         disaggregated.push(pasukobj);
     });
+
+
+    if (searchSeq.length !== 0) {
+        const occurenceText = totalOccurrences === 1 ? "1 occurrence." : `${totalOccurrences} occurrences.`;
+        $("#occurrences").text(occurenceText);
+    }
+    else {
+        $("#occurrences").text("");
+    }
 
     links = tree.links(nodes);
     update();
