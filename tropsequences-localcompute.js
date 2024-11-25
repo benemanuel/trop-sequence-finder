ff7fc5e235c2bf9b6ca5d3a04f67a5584e70a20e
Avi BenEmanuel <benemanuel@geulah.org.il>
2024-11-25 17:03:57 +0200

    update

--- tropsequences-localcompute.js
+++ tropsequences-localcompute.js
@@ -979,28 +979,4 @@ if (Cookies.get("firstload") != "no") {
 Cookies.set("firstload", "no", {
     expires: 7
 }
-
-window.onload = addListeners;
-
-function addListeners(){
-    document.getElementById('.circledemo').addEventListener('mousedown', mouseDown, false);
-    window.addEventListener('mouseup', mouseUp, false);
-
-}
-
-function mouseUp()
-{
-    window.removeEventListener('mousemove', divMove, true);
-}
-
-function mouseDown(e){
-  window.addEventListener('mousemove', divMove, true);
-}
-
-function divMove(e){
-    var div = document.getElementById('#detailsModal');
-  div.style.position = 'absolute';
-  div.style.top = e.clientY + 'px';
-  div.style.left = e.clientX + 'px';
-}
 );
